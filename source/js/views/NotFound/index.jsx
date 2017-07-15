c800418824caadc67e85317f7d34b129d0a3d2a8
Max Gaudin <maxwell.gaudin@gmail.com>
2017-07-15 03:35:50 -0500

    404 page

--- source/js/views/NotFound/index.jsx
+++ source/js/views/NotFound/index.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 export default class NotFound extends Component {
   render() {
     return (
-      <div className='NotFound'>
-        <h1>Not Found</h1>
+      <div className='NotFound text-center'>
+        <h1>Page not found</h1>
+        <img src='./assets/img/not-found.jpg' />
       </div>
     );
   }
