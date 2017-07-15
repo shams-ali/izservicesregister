e31468a3666fd9663057cf72ba5be923b51e5f79
Max Gaudin <maxwell.gaudin@gmail.com>
2017-07-15 04:15:01 -0500

    Styling

--- source/js/views/Applications/index.js
+++ source/js/views/Applications/index.js
@@ -21,7 +21,7 @@ class Applications extends Component {
             <div className='col-md-12 text-center'>
               <h3>Welcome to the Registration Invoice Generator</h3>
 
-              <h3>What do you need help with?</h3>
+              <h4>What do you need help with?</h4><br />
               {this.applications.map((application, index) => (
                 <div key={ application.title }>
                   <NavLink to={ `application/${ application.link }` } className='btn btn-default btn-lg btn-block' key={ index } >
